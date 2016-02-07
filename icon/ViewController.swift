7d4cd1e44b4c92f90ab014f8a08d10491b52c12f
hmlab book pro <saki.tommy69@gmail.com>
2016-02-07 16:26:36 +0900

    Initial Commit

---
+++ icon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  icon
+//
+//  Created by hmlab book pro on 2016/02/07.
+//  Copyright © 2016年 hmlab book pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
